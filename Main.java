0dfff3df4d7bbbd6e15cc42ba3e0c6517a1e52aa
Mary Korth <mkorth@udallas.edu>
2019-12-10 23:54:50 -0600

    Final Main

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 package com.company;
-//extends com.company.Scrabble;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scrabble game = new Scrabble();
-        //System.out.println(game);
         game.fillletterBag();
         game.play();
 
